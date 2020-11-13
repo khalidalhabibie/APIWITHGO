19a1e025648f206d9be29484737d089be5532041
khalidalhabibie <khalidalhabibie@rocketmail.com>
2020-11-13 15:03:45 +0700

    add some file

--- model/user_model.go
+++ model/user_model.go
@@ -4,6 +4,12 @@ import  "gorm.io/gorm"
 
 type User struct{
 	gorm.Model
-	Name 		string `"json:name"`
-	Email		string ''
+	Name 		string `"json:name"  binding:"required"`
+	Email		string  `json:"email" binding:"required/email gorm:"uniqe""`
+	Password 	string `json:"password" binding:"required"`
+
+}
+
+func (User) TableName() string{
+	return "users"
 }
