1d5c9043bf0b4abaa5247e14ef61d8b41cde3b72
khalidalhabibie <khalidalhabibie@rocketmail.com>
2020-11-15 14:01:08 +0700

    fix struct user model

--- model/user_model.go
+++ model/user_model.go
@@ -5,7 +5,7 @@ import  "gorm.io/gorm"
 type User struct{
 	gorm.Model
 	Name 		string `"json:"name"  binding:"required"`
-	Email		string  `json:"email" binding:"required,email" gorm:"uniqe`
+	Email		string  `json:"email" binding:"required/email" gorm:"uniqe`
 	Password 	string `json:"password" binding:"required"`
 
 }
