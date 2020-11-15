7db74c508ee5ae9b7f55311aaeb708d37a6e2d7b
khalidalhabibie <khalidalhabibie@rocketmail.com>
2020-11-15 18:57:14 +0700

    security not yet

--- Makefile
+++
@@ -1,6 +0,0 @@
-package:
-	go get -u github.com/gin-gonic/gin
-	go get github.com/joho/godotenv
-	go get -u gorm.io/gorm
-
-
--- repository/user-repository.go
+++ repository/user-repository.go
@@ -12,7 +12,6 @@ type UserRepository interface{
 	AddUser(model.User) (model.User, error)
 	GetUser(int)(model.User,error)
 	GetByEmail(string)(model.User, error)
-	
 	GetAllUser() ([]model.User, error)
 	UpdateUser(model.User) (model.User, error)
 	DeleteUser(model.User) (model.User, error)
